19b00baea8950e983c4fc2bf3ca38eb42e217f34
guylamd <guy.l@whalo.com>
2026-04-28 15:44:12 +0300

    Initial commit: add app and auth source files

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+
+import { buildGoogleAuthUrl } from "@/lib/google";
+
+export async function GET() {
+  return NextResponse.redirect(await buildGoogleAuthUrl());
+}
